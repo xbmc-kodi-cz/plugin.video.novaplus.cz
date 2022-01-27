7121e750fb27a2bb2f356fe1839b21674c6d3dcf
wombat01 <20426629+wombat01@users.noreply.github.com>
2022-01-27 13:33:43 +0100

    [plugin.video.novaplus.cz] 1.3.3+matrix
    Watch live

--- resources/lib/plugin.py
+++ resources/lib/plugin.py
@@ -53,7 +53,7 @@ def list_recent():
         if dur:
             dur = get_duration(article.find(
                 'span', {'class': 'e-duration'}).get_text())
-        list_item = xbmcgui.ListItem(label=title)
+        list_item = xbmcgui.ListItem(title)
         list_item.setInfo(
             'video', {'mediatype': 'episode', 'title': title, 'duration': dur})
         list_item.setArt({'icon': article.find(
@@ -67,7 +67,34 @@ def list_recent():
     xbmcplugin.endOfDirectory(plugin.handle)
 
 
-@plugin.route('/get_list/')
+@plugin.route('/list_live/')
+def list_live():
+    xbmcplugin.setContent(plugin.handle, 'tvshows')
+    soup = get_page(_baseurl+'sledujte-zive')
+    listing = []
+    articles = soup.find('ul', {
+                         'class': 'js-channels-navigation-carousel'}).findAll('li')
+
+    for article in articles:
+        if article.find('a', {'data-channel-id': re.compile(r"[0-9]+")}):
+            channel = article.find(
+                'span', {'class': 'e-logo'}).find('img')
+            show = article.find('h4').get_text()
+            title = channel['alt']
+            label = u'[COLOR blue]{}[/COLOR] · {}'.format(title, show)
+            list_item = xbmcgui.ListItem(label)
+            list_item.setInfo(
+                'video', {'mediatype': 'tvshow', 'tvshowtitle': title, 'title': show, 'plot': '{0} -{1}'.format(article.find('span', {'class': 'e-time-start'}).get_text(), article.find('span', {'class': 'e-time-end'}).get_text()), 'playcount': 0})
+            list_item.setArt({'poster': channel['src']})
+            list_item.setProperty('IsPlayable', 'true')
+            listing.append(
+                (plugin.url_for(get_live, article.find('a')['href']), list_item, False))
+
+    xbmcplugin.addDirectoryItems(plugin.handle, listing, len(listing))
+    xbmcplugin.endOfDirectory(plugin.handle)
+
+
+@ plugin.route('/get_list/')
 def get_list():
     xbmcplugin.setContent(plugin.handle, 'episodes')
     listing = []
@@ -109,7 +136,7 @@ def get_list():
     xbmcplugin.endOfDirectory(plugin.handle)
 
 
-@plugin.route('/get_category/')
+@ plugin.route('/get_category/')
 def get_category():
     listing = []
     soup = get_page(plugin.args['show_url'][0])
@@ -125,7 +152,7 @@ def get_category():
     xbmcplugin.endOfDirectory(plugin.handle)
 
 
-@plugin.route('/get_video/<path:url>')
+@ plugin.route('/get_video/<path:url>')
 def get_video(url):
     PROTOCOL = 'mpd'
     DRM = 'com.widevine.alpha'
@@ -151,7 +178,6 @@ def get_video(url):
             is_helper = inputstreamhelper.Helper(PROTOCOL, drm=DRM)
             if is_helper.check_inputstream():
                 stream_data = json_data['DASH'][0]
-                print(stream_data['type'])
                 list_item.setPath(stream_data['src'])
                 list_item.setContentLookup(False)
                 list_item.setMimeType('application/xml+dash')
@@ -170,6 +196,25 @@ def get_video(url):
                                       _addon.getLocalizedString(30006), xbmcgui.NOTIFICATION_ERROR, 5000)
 
 
+@ plugin.route('/get_live/<path:url>')
+def get_live(url):
+    soup = get_page(url)
+    embeded = get_page(soup.find(
+        'div', {'class': 'b-image'}).find('iframe')['src']).find_all('script')[-1]
+
+    json_data = json.loads(re.compile(
+        '{\"tracks\":(.+?),\"duration\"').findall(str(embeded))[0])
+    if json_data:
+        stream_data = json_data['HLS'][0]
+        list_item = xbmcgui.ListItem()
+        list_item.setPath(
+            stream_data['src']+'|Referer=https://media.cms.nova.cz/')
+        xbmcplugin.setResolvedUrl(plugin.handle, True, list_item)
+    else:
+        xbmcgui.Dialog().notification(_addon.getAddonInfo('name'),
+                                      _addon.getLocalizedString(30006), xbmcgui.NOTIFICATION_ERROR, 5000)
+
+
 def get_duration(dur):
     duration = 0
     l = dur.strip().split(':')
@@ -180,13 +225,18 @@ def get_duration(dur):
 
 def get_page(url):
     r = requests.get(url, headers={
-                     'User-Agent': 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'})
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.100.4758.66 Safari/537.36'})
     return BeautifulSoup(r.content, 'html.parser')
 
 
-@plugin.route('/')
+@ plugin.route('/')
 def root():
     listing = []
+
+    list_item = xbmcgui.ListItem("Živě")
+    list_item.setArt({'icon': 'DefaultAddonPVRClient.png'})
+    listing.append((plugin.url_for(list_live), list_item, True))
+
     list_item = xbmcgui.ListItem(_addon.getLocalizedString(30001))
     list_item.setArt({'icon': 'DefaultRecentlyAddedEpisodes.png'})
     listing.append((plugin.url_for(list_recent), list_item, True))
